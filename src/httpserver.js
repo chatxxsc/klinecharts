e9e84ee9d2687d7ecc29546ab366e25d11c8dea1
chatxxsc <62228768+chatxxsc@users.noreply.github.com>
2024-07-13 10:24:32 +0800

    Create httpserver.js

---
+++ src/httpserver.js
@@ -0,0 +1,141 @@
+import express from "../node_modules/express/index.js";
+// import { createProxyMiddleware } from "../node_modules/http-proxy-middleware/dist/index.js";
+import axios from "axios";
+import { WebSocketServer } from "ws";
+import WebSocket from "ws"; // To create client WebSocket
+
+const app = express();
+
+// Middleware to handle CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://27vrr7-5173.csb.app");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  // Handle OPTIONS method
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+app.use("/api", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://www.okx.com/api/v5/public/instruments?instType=SPOT",
+    );
+    const data = response.data.data.map((a) => ({
+      ticker: a.instId,
+      name: a.name,
+      shortName: a.baseCcy,
+      market: "okx",
+      exchange: a.baseCcy,
+      type: a.instType,
+    }));
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data from OKX API:", error);
+    res.status(500).send("Error fetching data from OKX API");
+  }
+});
+//https://www.okx.com/priapi/v5/market/candles?instId=BTC-USDT-SWAP&before=1720825526000&bar=1m&t=1720833506036
+
+app.use("/kline-history", async (req, res) => {
+  const { instId, bar, before, after } = req.query;
+
+  if (!instId || !bar || !before || !after) {
+    return res.status(400).send("Missing required query parameters");
+  }
+
+  const apiUrl = `https://www.okx.com/priapi/v5/market/candles?instId=${instId}-SWAP&bar=${bar}&before=${before}&after=${after}&limit=200`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    const data = response.data.data;
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data from OKX API:", error);
+    res.status(500).send("Error fetching data from OKX API");
+  }
+});
+app.listen(3000, () => {
+  console.log("CORS proxy server is running on port 3000");
+});
+
+const server = app.listen(3001, () => {
+  console.log("CORS proxy server is running on port 3001");
+});
+
+// WebSocket Server
+const wss = new WebSocketServer({ server, path: "/ws" });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected to WebSocket server");
+
+  // Connect to the target WebSocket server
+  const targetWs = new WebSocket("wss://ws.okx.com:8443/ws/v5/business");
+
+  // Forward messages from client to target WebSocket server
+  ws.on("message", (message) => {
+    if (Buffer.isBuffer(message)) {
+      const textMessage = message.toString();
+      targetWs.send(textMessage);
+    } else {
+      targetWs.send(message);
+    }
+  });
+
+  // Handle messages from target WebSocket server
+  targetWs.on("message", (message) => {
+    // Convert binary message to text
+    if (Buffer.isBuffer(message)) {
+      const textMessage = message.toString();
+      console
+        .log
+        // `Message from target WebSocket server (converted to text): ${textMessage}`,
+        ();
+      ws.send(textMessage);
+    } else {
+      // console.log(`Message from target WebSocket server: ${message}`);
+      ws.send(message);
+    }
+  });
+
+  // Handle target WebSocket server connection close
+  targetWs.on("close", () => {
+    console.log("Target WebSocket server connection closed");
+    ws.close();
+  });
+
+  // Handle target WebSocket server errors
+  targetWs.on("error", (error) => {
+    console.error("Error in target WebSocket server:", error);
+    ws.close();
+  });
+
+  // Handle client connection close
+  ws.on("close", () => {
+    console.log("Client connection closed");
+    targetWs.close();
+  });
+
+  // Handle client errors
+  ws.on("error", (error) => {
+    console.error("Error in client WebSocket connection:", error);
+    targetWs.close();
+  });
+
+  // const subscribeMessage = JSON.stringify({
+  //   op: "subscribe",
+  //   args: [{ channel: "candle1m", instId: "BTC-USDT" }],
+  // });
+  // targetWs.on("open", () => {
+  //   console.log("Connected to target WebSocket server");
+  //   targetWs.send(subscribeMessage);
+  // });
+});
